e1e9c7a9e681f86b5e2c2eac2a6fca7758ae6c5f
Larry Lopez <154121+namralkeeg@users.noreply.github.com>
2018-09-10 16:12:32 -0700

    Added non-cryptographic ELF32 hash algorithm and tests.

---
+++ Src/Hashlib.NET.Tests/NonCryptographic/Elf32Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using Hashlib.NET.NonCryptographic;
+using Xunit;
+
+namespace Hashlib.NET.Tests.NonCryptographic
+{
+    public class Elf32Tests
+    {
+        public class MultipleBytesArray
+        {
+            private readonly Elf32 _hashAlgorithm;
+
+            public MultipleBytesArray()
+            {
+                _hashAlgorithm = Elf32.Create();
+            }
+
+            [Theory]
+            [InlineData(0x0006d652u, "foob")]
+            [InlineData(0x006d6581u, "fooba")]
+            [InlineData(0x06d65882u, "foobar")]
+            public void ComputeHash_WithMultipleBytes(uint expected, string testString)
+            {
+                _hashAlgorithm.Initialize();
+                var bytes = Encoding.UTF8.GetBytes(testString);
+                var result = _hashAlgorithm.ComputeHash(bytes, 0, bytes.Length);
+                var hash = BitConverter.ToUInt32(result, 0);
+                Assert.Equal(expected, hash);
+            }
+        }
+    }
+}
