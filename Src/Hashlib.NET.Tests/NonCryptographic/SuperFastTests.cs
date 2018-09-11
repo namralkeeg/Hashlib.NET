88fe484ff2080c788580a47e5ce490e782482e33
Larry Lopez <154121+namralkeeg@users.noreply.github.com>
2018-09-11 15:47:02 -0700

    Added non-cryptographic SuperFast hash algorithm and tests.

---
+++ Src/Hashlib.NET.Tests/NonCryptographic/SuperFastTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using Hashlib.NET.NonCryptographic;
+using Xunit;
+
+namespace Hashlib.NET.Tests.NonCryptographic
+{
+    public class SuperFastTests
+    {
+        public class MultipleBytesArray
+        {
+            private readonly SuperFast _hashAlgorithm;
+
+            public MultipleBytesArray()
+            {
+                _hashAlgorithm = SuperFast.Create();
+            }
+
+            [Theory]
+            [InlineData(0x58C9CBD5u, "foob")]
+            [InlineData(0xF9D871C8u, "fooba")]
+            [InlineData(0xA6BCDCA9u, "foobar")]
+            public void ComputeHash_WithMultipleBytes(uint expected, string testString)
+            {
+                var bytes = Encoding.UTF8.GetBytes(testString);
+                var result = _hashAlgorithm.ComputeHash(bytes, 0, bytes.Length);
+                var hash = BitConverter.ToUInt32(result, 0);
+                Assert.Equal(expected, hash);
+            }
+        }
+    }
+}
