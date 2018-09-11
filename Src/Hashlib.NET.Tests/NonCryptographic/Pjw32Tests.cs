ddf7b2c808f66234f9eeea647ba34b6c7972f9bf
Larry Lopez <154121+namralkeeg@users.noreply.github.com>
2018-09-11 00:28:22 -0700

    Added non-cryptographic PJW 32-bit hash algorithm and tests.

---
+++ Src/Hashlib.NET.Tests/NonCryptographic/Pjw32Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using Hashlib.NET.NonCryptographic;
+using Xunit;
+
+namespace Hashlib.NET.Tests.NonCryptographic
+{
+    public class Pjw32Tests
+    {
+        public class MultipleBytesArray
+        {
+            private readonly Pjw32 _hashAlgorithm;
+
+            public MultipleBytesArray()
+            {
+                _hashAlgorithm = Pjw32.Create();
+            }
+
+            [Theory]
+            [InlineData(0x0006d652u, "foob")]
+            [InlineData(0x006d6581u, "fooba")]
+            [InlineData(0x06d65882u, "foobar")]
+            public void ComputeHash_WithMultipleBytes(uint expected, string testString)
+            {
+                var bytes = Encoding.UTF8.GetBytes(testString);
+                var result = _hashAlgorithm.ComputeHash(bytes, 0, bytes.Length);
+                var hash = BitConverter.ToUInt32(result, 0);
+                Assert.Equal(expected, hash);
+            }
+        }
+    }
+}
