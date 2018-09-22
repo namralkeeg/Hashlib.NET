0ac4b0b313531639ccb976f81da90b025a8dab56
Larry Lopez <154121+namralkeeg@users.noreply.github.com>
2018-09-21 17:11:31 -0700

    Added IBlockHash interface, and implemented it on each supported cryptographic hash algorithm class.

---
+++ Src/Hashlib.NET/Common/IBlockHash.cs
@@ -0,0 +1,13 @@
+﻿namespace Hashlib.NET.Common
+{
+    /// <summary>
+    /// An Interface for hash algorithms that use a block algorithm.
+    /// </summary>
+    internal interface IBlockHash
+    {
+        /// <summary>
+        /// The size in bytes of each block that's processed at once.
+        /// </summary>
+        int BlockSize { get; }
+    }
+}
